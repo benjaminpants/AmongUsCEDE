5a7236b2ac58cfd7723999ee826009d645fcb6d4
benjaminpants <maltbyfamily@gmail.com>
2021-10-22 12:50:01 -0700

    reminder to actually work when i get back

---
+++ AmongUsCEDE/Core/Patches/Main/BodyPatches.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using HarmonyLib;
+
+namespace AmongUsCEDE.Core.Patches
+{
+
+	[HarmonyPatch(typeof(DeadBody))]
+	[HarmonyPatch("OnClick")]
+	class BodyPatches
+	{
+		static bool Prefix()
+		{
+			//TODO: finish this
+			return true;
+		}
+	}
+}
