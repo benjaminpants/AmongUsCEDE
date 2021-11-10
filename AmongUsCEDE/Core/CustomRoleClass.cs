1ce63290c5f9fb889e080632c6c45aa9d8d12f97
benjaminpants <maltbyfamily@gmail.com>
2021-11-09 22:19:50 -0800

    minor setup

---
+++ AmongUsCEDE/Core/CustomRoleClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AmongUsCEDE;
+
+namespace AmongUsCEDE.Core
+{
+	class CustomRoleClass : RoleBehaviour
+	{
+		//NOTES: Make IsImpostor always return false
+	}
+}
