4465ce289ecffa289c9f2c55188205fd6a7231c9
benjaminpants <maltbyfamily@gmail.com>
2021-11-10 12:20:53 -0800

    Update CustomRoleClass.cs

--- AmongUsCEDE/Core/CustomRoleClass.cs
+++ AmongUsCEDE/Core/CustomRoleClass.cs
@@ -22,6 +22,31 @@ namespace AmongUsCEDE.Core
 			}
 		}
 
+		public override void Initialize(PlayerControl player)
+		{
+			//We gotta assume role was defined earlier or we're screwed
+			this.Player = player;
+			if (!player.AmOwner)
+			{
+				return;
+			}
+			if (this.CanUseKillButton)
+			{
+				DestroyableSingleton<HudManager>.Instance.KillButton.Show();
+				DestroyableSingleton<HudManager>.Instance.SabotageButton.Show();
+				DestroyableSingleton<HudManager>.Instance.ImpostorVentButton.Show();
+				player.SetKillTimer(10f);
+			}
+			else
+			{
+				DestroyableSingleton<HudManager>.Instance.KillButton.Hide();
+				DestroyableSingleton<HudManager>.Instance.SabotageButton.Hide();
+				DestroyableSingleton<HudManager>.Instance.ImpostorVentButton.Hide();
+			}
+			player.nameText.color = this.NameColor;
+			this.InitializeAbilityButton();
+		}
+
 
 	}
 
